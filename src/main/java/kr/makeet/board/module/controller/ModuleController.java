668b3977b29745fc17a60b7a1b8992b7080b9f59
recflow <chjh9107@gmail.com>
2022-03-31 02:40:15 +0900

    p.121

--- src/main/java/kr/makeet/board/module/controller/ModuleController.java
+++ src/main/java/kr/makeet/board/module/controller/ModuleController.java
@@ -55,7 +55,7 @@ public class ModuleController {
         log.info("ex3");
     }
 
-    @GetMapping({"/exLayout1","/exLayout2","/exTemplate"})
+    @GetMapping({"/exLayout1","/exLayout2","/exTemplate","/exSidebar"})
     public void exLayout1(){
         log.info("exLayout.........");
     }
